a1104c91ced0302d4ff13db449b6708d7c8e7d54
ArielCalisaya <calisayaariel62@gmail.com>
2020-07-28 14:07:09 -0400

    +MuiTheme +defaulPaletteColors

--- src/App.js
+++ src/App.js
@@ -1,20 +1,31 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import home from './pages/home'
-import clothes from './pages/clothes'
+import home from "./pages/home";
+import clothes from "./pages/clothes";
 import "./App.css";
 
+// Material Default theme and personalization
+import { ThemeProvider as MuiThemeProvider } from "@material-ui/core/styles";
+import createMuiTheme from "@material-ui/core/styles/createMuiTheme";
+import customTheme from "./utils/theme";
+
+const theme = createMuiTheme(customTheme)
 function App() {
+  
+
   return (
+    <MuiThemeProvider theme={theme}>
+
     <Router>
       <Switch>
         <div className="App">
-        <h1>Confecciones Rimba</h1>
-        <Route exact path="/" component={home}/>
-        <Route path="/ropa" component={clothes}/>
+          <h1>Confecciones Rimba</h1>
+          <Route exact path="/" component={home} />
+          <Route path="/ropa" component={clothes} />
         </div>
       </Switch>
     </Router>
+    </MuiThemeProvider>
   );
 }
 
---
+++ src/utils/theme.js
@@ -0,0 +1,21 @@
+export default {
+  palette: {
+    primary: {
+      light: "#99AAB5",
+      main: "#7289DA",
+      dark: "#002884",
+      contrastText: "#fff",
+    },
+    secondary: {
+      light: "#99AAB5",
+      main: "#23272A",
+      dark: "#000000",
+      contrastText: "#fff",
+    },
+  },
+  styled: {
+    form: {
+      textAlign: "center",
+    },
+  },
+};
