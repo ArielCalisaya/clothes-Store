fcee75361030e7f1b5257c47b4e469fa2e9d03f7
ArielCalisaya <calisayaariel62@gmail.com>
2020-07-28 13:40:07 -0400

    -unused_files +routes_and_pages

---
+++ src/pages/clothes.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const clothes = () => {
+  return (
+    <div>
+      <h3>Preziooooo beciiii ropaaa</h3>
+
+      <Link to="/">
+        <p>home</p>
+      </Link>
+    </div>
+  );
+};
+export default clothes;
---
+++ src/pages/home.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const home = () => {
+  return (
+    <div>
+        Pagina de iniciooooooooooooooo
+      <Link to="/ropa">
+        <p>Ropa</p>
+      </Link>
+    </div>
+  );
+};
+export default home;
