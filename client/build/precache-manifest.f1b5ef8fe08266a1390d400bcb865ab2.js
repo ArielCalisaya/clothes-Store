ed20465e44e13031e7dcfb2fefc2e0882ba0c4ba
ArielC <calisayaariel62@gmail.com>
2020-08-17 23:34:37 -0400

    push heroku

---
+++ client/build/precache-manifest.f1b5ef8fe08266a1390d400bcb865ab2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3cdf7fba45e752c432e71fee15be5330",
+    "url": "/index.html"
+  },
+  {
+    "revision": "500e6318620c2d276bab",
+    "url": "/static/css/main.7dc25c86.chunk.css"
+  },
+  {
+    "revision": "51279681db596118ba65",
+    "url": "/static/js/2.0becbc9c.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.0becbc9c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "500e6318620c2d276bab",
+    "url": "/static/js/main.c6067d39.chunk.js"
+  },
+  {
+    "revision": "9bc550e76590c627899e",
+    "url": "/static/js/runtime-main.9335457d.js"
+  },
+  {
+    "revision": "5c4a8cf2fcce4715782360bde2cf9f91",
+    "url": "/static/media/bg.5c4a8cf2.jpg"
+  }
+]);
